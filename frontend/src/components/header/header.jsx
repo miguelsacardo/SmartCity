2cf8a130c9dbc6f95e5a410f7f356ede208ea119
miguelsacardo <miguelsacardo3@gmail.com>
2025-05-23 13:27:23 -0300

    header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,19 +1,19 @@
 export function Header() {
   return (
-    <header className="h-[5.063rem] bg-[#392161]">
-      <div className="">
+    <header className="flex items-center justify-between h-[5.063rem] bg-[#392161] pl-[3.438rem] pr-[3.438rem]">
         <div className="h-[3.75rem] w-[3.75rem]">
-          <img
-            src="/logo_header.png"
-            alt="logo do site contendo 4 bordas formando um quadrado cercando um sensor com um desenho de sinal do wifi. Você pode clicar aqui para voltar para a página inicial do site!"
-            className="w-full"
-          />
+          <a href="">
+            <img
+              src="/logo_header.png"
+              alt="logo do site contendo 4 bordas formando um quadrado cercando um sensor com um desenho de sinal do wifi. Você pode clicar aqui para voltar para a página inicial do site!"
+              className="w-full"
+            />
+          </a>
         </div>
 
         <div className="h-[3.125rem] w-[3.125rem]">
           <img src="/icons/user_icon.png" alt="" className="w-full" />
         </div>
-      </div>
     </header>
   );
 }
